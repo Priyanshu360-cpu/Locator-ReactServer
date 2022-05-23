3b8a5a1ebbd480bf52700d85124d9e4a02abc973
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-05-23 13:55:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      textAreaValue: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ textAreaValue: event.target.value });
+  }
+
+  render(){return (
+    <div className="background">
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <input type="textarea" 
+          name="textValue"
+          onChange={this.handleChange}
+        />
         </p>
         <a
           className="App-link"
@@ -19,7 +37,9 @@ function App() {
         </a>
       </header>
     </div>
+    </div>
   );
+}
 }
 
 export default App;

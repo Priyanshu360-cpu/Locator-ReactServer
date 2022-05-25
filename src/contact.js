f130cb0cfd0b5df6f1230d020670ab2a6dc1b090
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-05-26 00:27:40 +0530

    updated methods

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h1>Page ID - {window.location.href.split("id=")[1]}</h1>
+      <h1>Page ID - {window.location.href.split("?id=")[1]}</h1>
     </div>
   );
 };

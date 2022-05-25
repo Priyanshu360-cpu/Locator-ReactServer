61698c81351c9a6f3df1e1b8fb990df90e4e4bc7
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-05-25 22:49:57 +0530

    added taskbar

--- src/homepage.js
+++ src/homepage.js
@@ -17,6 +17,7 @@ class App extends Component {
 
   render(){return (
     <div className="background">
+      <div class="left"><div class="sidemenu"><image src="https://cdn.discordapp.com/emojis/970780591169630278.png?size=32"></image></div><div class="imaga"><a href="https://play.google.com/store"><image src="https://cdn.discordapp.com/emojis/824189692496969740.png?size=32"></image></a></div><div class="imagai"><a href="https://www.instagram.com/priyanshu.__0007/"><image src="https://cdn.discordapp.com/emojis/797052330054516747.gif?size=32"></image></a></div><div class="imagag"><a href="mailto:tpriyanshu775@gmail.com"><image src="https://cdn.discordapp.com/emojis/775178764715950100.gif?size=64"></image></a></div></div>
     <div className="App">
       <header className="App-header">
         <img src="https://cdn-icons-png.flaticon.com/512/854/854878.png" className="App-logo" alt="logo" />
@@ -27,7 +28,7 @@ class App extends Component {
         <p><input type="textarea" 
           name="textValue"
           onChange={this.handleChange}
-        /><button value="Go" onClick={() => window.open('/'+this.state.textAreaValue)}>GO</button></p>
+        /><button class="button button1" value="Go" onClick={() => window.open('/'+this.state.textAreaValue)}>GO</button></p>
         <a
           className="App-link"
           href="/contact"

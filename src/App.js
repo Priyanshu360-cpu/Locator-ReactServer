a5948839cdbd76ea49f008e87a37356debf6466f
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-05-28 20:23:12 +0530

    accustomed realm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Contact from './contact';
-
+import Realmdb from './realmdb';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Home from './homepage';
@@ -12,6 +12,7 @@ return (
     <Routes>
         <Route exact path='/' exact element={<Home />} />
         <Route path='/mapview' element={<Contact/>} />
+        <Route path='/realm' element={<Realmdb/>} />
     </Routes>
     </Router>
 );

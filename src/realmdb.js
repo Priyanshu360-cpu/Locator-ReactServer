52a641956a6e9b56f998b68208e7c37ec05e1985
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-05-28 20:17:55 +0530

    realm db initialised

---
+++ src/realmdb.js
@@ -0,0 +1,30 @@
+import * as Realm from "realm-web";
+const app = new Realm.App({ id: APP_ID });
+
+function UserDetail({ user }) {
+    return (
+      <div>
+        <h1>Logged in with anonymous id: {user.id}</h1>
+      </div>
+    );
+  }
+
+  function Login({ setUser }) {
+    const loginAnonymous = async () => {
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user);
+    };
+    return <button onClick={loginAnonymous}>Log In</button>;
+  }
+  const App = () => {
+    const [user, setUser] = React.useState(app.currentUser);
+    return (
+      <div className="App">
+        <div className="App-header">
+          {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+        </div>
+      </div>
+    );
+  };
+  export default App;
+  

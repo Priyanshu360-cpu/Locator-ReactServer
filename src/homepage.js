92e2fbd78b34f27d3a41bdf1edd741ad0582d944
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-02 04:12:15 +0530

    Updated StyleSheet

--- src/homepage.js
+++ src/homepage.js
@@ -38,7 +38,7 @@ class App extends Component {
   render(){return (
     <div className="background">
     <div className="App">
-    <div class="left"><div class="sidemenu"><image src="https://cdn.discordapp.com/emojis/970780591169630278.png?size=32"></image> <div class="hide">hi</div></div><div class="imaga"><a href="https://play.google.com/store"><image src="https://cdn.discordapp.com/emojis/824189692496969740.png?size=32"></image></a></div><div class="imagai"><a href="https://www.instagram.com/priyanshu.__0007/"><image src="https://cdn.discordapp.com/emojis/797052330054516747.gif?size=32"></image></a></div><div class="imagag"><a href="mailto:tpriyanshu775@gmail.com"><image src="https://cdn.discordapp.com/emojis/775178764715950100.gif?size=64"></image></a></div></div>
+    <div class="left"><div class="sidemenu"><img src="https://cdn.discordapp.com/emojis/970780591169630278.png?size=32"></img> <div class="hide">hi</div></div><div class="imaga"><a href="https://play.google.com/store"><img src="https://cdn.discordapp.com/emojis/824189692496969740.png?size=32"></img></a></div><div class="imagai"><a href="https://www.instagram.com/priyanshu.__0007/"><img src="https://cdn.discordapp.com/emojis/797052330054516747.gif?size=32"></img></a></div><div class="imagag"><a href="mailto:tpriyanshu775@gmail.com"><img src="https://cdn.discordapp.com/emojis/775178764715950100.gif?size=64"></img></a></div></div>
    
       <header className="App-header">
         <img src="https://cdn-icons-png.flaticon.com/512/854/854878.png" className="App-logo" alt="logo" />
